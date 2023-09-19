7efe1a50a3f94275d21121e35103ca3a7dddb201
Izet Delibasic <izetdelibasic55@gmail.com>
2023-09-19 02:02:42 +0200

    Adding quiz component

---
+++ src/components/Trivia.jsx
@@ -0,0 +1,13 @@
+export default function Trivia() {
+  return (
+        <div className="trivia">
+            <div className="question">What atmospheric component plays a crucial role in protecting Earth from harmful solar radiation?</div>
+            <div className="answers">
+                <div className="answer">Nitrogen</div>
+                <div className="answer">Oxygen</div>
+                <div className="answer">Ozone</div>
+                <div className="answer">Argon</div>
+            </div>
+        </div>
+  )
+}
