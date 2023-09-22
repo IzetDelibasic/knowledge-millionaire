9dba6aa2ef60f87c0319da372bd51ab08ab9503a
Izet Delibasic <izetdelibasic55@gmail.com>
2023-09-22 02:00:51 +0200

    Adding some new features and finishing app style

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,44 @@
-import { useState } from "react";
 import "./app.css";
+import { useEffect, useMemo, useState } from "react";
+import Start from "./components/Start";
+import Timer from "./components/Timer";
 import Trivia from "./components/Trivia";
 
 function App() {
+  const [username, setUsername] = useState(null);
+  const [timeOut, setTimeOut] = useState(false);
   const [questionNumber, setQuestionNumber] = useState(1);
-  const moneyPyramid = [
+  const [earned, setEarned] = useState("$ 0");
+
+  const data = [
+    {
+      id: 1,
+      question: "Who is the author of the book 'To Kill a Mockingbird'? ",
+      answers: [
+        {
+          text: "Charles Dickens",
+          correct: false,
+        },
+        {
+          text: "Harper Lee",
+          correct: true,
+        },
+        {
+          text: " William Shakespeare",
+          correct: false,
+        },
+        {
+          text: "J.K. Rowling",
+          correct: false,
+        },
+      ],
+    },
+    
+  ];
+
+  const moneyPyramid = useMemo(
+    () =>
+      [
         { id: 1, amount: "$ 100" },
         { id: 2, amount: "$ 200" },
         { id: 3, amount: "$ 300" },
@@ -20,26 +54,63 @@ function App() {
         { id: 13, amount: "$ 250.000" },
         { id: 14, amount: "$ 500.000" },
         { id: 15, amount: "$ 1.000.000" },
-      ].reverse();
-  
+      ].reverse(),
+    []
+  );
+
+  useEffect(() => {
+    questionNumber > 1 &&
+      setEarned(moneyPyramid.find((m) => m.id === questionNumber - 1).amount);
+  }, [questionNumber, moneyPyramid]);
+
   return (
     <div className="app">
-      <div className="main">
-        <div className="top">
-          <div className="timer">30</div>
-        </div>
-        <div className="bottom"><Trivia /></div>
-      </div>
-      <div className="pyramid">
-        <ul className="moneyList">
-          {moneyPyramid.map((m) =>(
-            <li className={questionNumber === m.id ? "moneyListItem active" : "moneyListItem"}>
-              <span className="moneyListItemNumber">{m.id}</span>
-              <span className="moneyListItemAmount">{m.amount}</span>
-            </li>
-          ))}
-        </ul>
-      </div>
+      {!username ? (
+        <Start setUsername={setUsername} />
+      ) : (
+        <>
+          <div className="main">
+            {timeOut ? (
+              <h1 className="endText">You earned: {earned}</h1>
+            ) : (
+              <>
+                <div className="top">
+                  <div className="timer">
+                    <Timer
+                      setTimeOut={setTimeOut}
+                      questionNumber={questionNumber}
+                    />
+                  </div>
+                </div>
+                <div className="bottom">
+                  <Trivia
+                    data={data}
+                    questionNumber={questionNumber}
+                    setQuestionNumber={setQuestionNumber}
+                    setTimeOut={setTimeOut}
+                  />
+                </div>
+              </>
+            )}
+          </div>
+          <div className="pyramid">
+            <ul className="moneyList">
+              {moneyPyramid.map((m) => (
+                <li
+                  className={
+                    questionNumber === m.id
+                      ? "moneyListItem active"
+                      : "moneyListItem"
+                  }
+                >
+                  <span className="moneyListItemNumber">{m.id}</span>
+                  <span className="moneyListItemAmount">{m.amount}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </>
+      )}
     </div>
   );
 }
