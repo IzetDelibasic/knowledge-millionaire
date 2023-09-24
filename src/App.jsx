98b130e086502d36869fde2c1781b3adc31c93cf
Izet Delibasic <izetdelibasic55@gmail.com>
2023-09-24 18:57:20 +0200

    Updating and adding new questions for game

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,315 @@ function App() {
         },
       ],
     },
-    
+    {
+      id: 2,
+      question: "Which planet is closest to the Sun? ",
+      answers: [
+        {
+          text: "Venus",
+          correct: true,
+        },
+        {
+          text: "Mars",
+          correct: false,
+        },
+        {
+          text: "Saturn",
+          correct: false,
+        },
+        {
+          text: "Jupiter",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 3,
+      question: "What is the capital city of France? ",
+      answers: [
+        {
+          text: "London",
+          correct: false,
+        },
+        {
+          text: "Rome",
+          correct: false,
+        },
+        {
+          text: "Madrid",
+          correct: false,
+        },
+        {
+          text: "Paris",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 4,
+      question: "Which river flows through Egypt? ",
+      answers: [
+        {
+          text: "Nile",
+          correct: true,
+        },
+        {
+          text: "Amazon",
+          correct: false,
+        },
+        {
+          text: "Mississippi",
+          correct: false,
+        },
+        {
+          text: "Volga",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 5,
+      question: "Which of these gases is the most abundant in Earth's atmosphere? ",
+      answers: [
+        {
+          text: "Oxygen",
+          correct: false,
+        },
+        {
+          text: "Hydrogen",
+          correct: false,
+        },
+        {
+          text: "Helium",
+          correct: false,
+        },
+        {
+          text: "Nitrogen",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 6,
+      question: "Who wrote the play 'Romeo and Juliet'? ",
+      answers: [
+        {
+          text: "Charles Dickens",
+          correct: false,
+        },
+        {
+          text: "William Shakespeare",
+          correct: true,
+        },
+        {
+          text: "Jane Austen",
+          correct: false,
+        },
+        {
+          text: "Mark Twain",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 7,
+      question: "Which of these is the smallest ocean in the world? ",
+      answers: [
+        {
+          text: "Atlantic Ocean",
+          correct: false,
+        },
+        {
+          text: "Indian Ocean",
+          correct: false,
+        },
+        {
+          text: "Arctic Ocean",
+          correct: true,
+        },
+        {
+          text: "Pacific Ocean",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 8,
+      question: "Who has won the most Grand Slam tennis tournaments in men's singles? ",
+      answers: [
+        {
+          text: "Rafael Nadal",
+          correct: false,
+        },
+        {
+          text: "Novak Djokovic",
+          correct: true,
+        },
+        {
+          text: "Roger Federer",
+          correct: false,
+        },
+        {
+          text: "Pete Sampras",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 9,
+      question: "Which country is hosting the 2022 FIFA World Cup? ",
+      answers: [
+        {
+          text: "Brazil",
+          correct: false,
+        },
+        {
+          text: "France",
+          correct: false,
+        },
+        {
+          text: "Qatar",
+          correct: true,
+        },
+        {
+          text: "Germany",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 10,
+      question: "Which planet is known as the 'Morning Star' and the 'Evening Star'? ",
+      answers: [
+        {
+          text: "Mars",
+          correct: false,
+        },
+        {
+          text: "Saturn",
+          correct: false,
+        },
+        {
+          text: "Venus",
+          correct: true,
+        },
+        {
+          text: "Mercury",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 11,
+      question: "Who wrote the novel '1984'? ",
+      answers: [
+        {
+          text: "George Orwell",
+          correct: true,
+        },
+        {
+          text: "Aldous Huxley",
+          correct: false,
+        },
+        {
+          text: "F. Scott Fitzgerald",
+          correct: false,
+        },
+        {
+          text: "Ernest Hemingway",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 12,
+      question: "Which country is the largest by land area? ",
+      answers: [
+        {
+          text: "Russia",
+          correct: true,
+        },
+        {
+          text: "China",
+          correct: false,
+        },
+        {
+          text: "United States",
+          correct: false,
+        },
+        {
+          text: "Canada",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 13,
+      question: "Which river flows through Rome? ",
+      answers: [
+        {
+          text: "Nile",
+          correct: false,
+        },
+        {
+          text: "Tisza",
+          correct: false,
+        },
+        {
+          text: "Thames",
+          correct: false,
+        },
+        {
+          text: "Tiber",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 14,
+      question: "Who is the author of the book 'The Great Gatsby'? ",
+      answers: [
+        {
+          text: "Charles Dickens",
+          correct: false,
+        },
+        {
+          text: "William Faulkner",
+          correct: false,
+        },
+        {
+          text: "F. Scott Fitzgerald",
+          correct: true,
+        },
+        {
+          text: "Ernest Hemingway",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 15,
+      question: "Who was the first person to set foot on the Moon? ",
+      answers: [
+        {
+          text: "Yuri Gagarin",
+          correct: false,
+        },
+        {
+          text: "John Glenn",
+          correct: false,
+        },
+        {
+          text: "Neil Armstrong",
+          correct: true,
+        },
+        {
+          text: "Buzz Aldrin",
+          correct: false,
+        },
+      ],
+    },
+
   ];
 
   const moneyPyramid = useMemo(
